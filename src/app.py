a4444c610b694fb94ffd65a156d62a0869bda8be
KLukyanchikov <mrwithoutnickname@gmail.com>
2023-03-28 15:14:34 +0000

    add static files

--- src/app.py
+++ src/app.py
@@ -2,16 +2,20 @@ import os
 
 import psycopg2
 from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory='templates')
 
 
-@app.get('/')
-def root():
-    return get_template()
+@app.get('/', response_class=HTMLResponse)
+def root(request: Request):
+    return get_template(request)
 
 
-def get_template():
+def get_text():
     conn = psycopg2.connect(os.environ['PG_DSN'])
     try:
         cur = conn.cursor()
@@ -22,3 +26,7 @@ def get_template():
     # connection usage
     finally:
         conn.close()
+
+
+def get_template(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request, 'text': get_text()})
